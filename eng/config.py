e1bba7b614dcea0475bbda605d7b8fe486c8c858
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2024-10-19 00:39:33 -0700

    Add base configuration classes.

---
+++ eng/config.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+
+# TODO: Send `today` to the templates.
+
+
+@dataclass
+class BaseConfig:
+    """
+    The base configuration for a newspaper.
+    """
+
+    website_url: str
+    website_display_url: str
+
+    header_image_url: str
+
+    tip_url: str
+    donate_url: str
+    submit_url: str
+
+
+@dataclass
+class BaseSocialConfig:
+    """
+    The configuration of social links for a newspaper.
+    """
+
+
+# Links and images as of 2024-10-18.
+THE_DAILY_BASE_CONFIG = BaseConfig(
+    website_url="https://stanforddaily.com/",
+    website_display_url="stanforddaily.com",
+    header_image_url="https://stanforddaily.com/wp-content/uploads/2019/11/cropped-DailyLogo-CardinalRed.png",
+    tip_url="https://stanforddaily.com/tips/",
+    donate_url="https://stanforddaily.com/donate/",
+    submit_url="https://stanforddaily.com/submitting-to-the-daily/",
+)
