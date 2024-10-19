3571fcb22fec05db14b99aa60961fe983ec7577e
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2024-10-19 16:16:37 -0700

    Begin `eng.generate`.

---
+++ eng/generate.py
@@ -0,0 +1,27 @@
+"""
+Generate MJML for newsletters
+and convert that MJML to HTML for an email.
+"""
+
+import pathlib
+
+import jinja2
+
+from eng.config import THE_DAILY_BASE_CONFIG
+from eng.config import THE_DAILY_BASE_DESIGN_CONFIG
+
+
+PACKAGE_NAME = "eng"
+
+
+def generate_mjml() -> str:
+    """
+    Generate MJML for a newsletter.
+    """
+
+
+def generate_html(mjml: str) -> str:
+    """
+    Generate HTML from MJML source for a newsletter.
+    This function expects that MJML has been installed as required.
+    """
