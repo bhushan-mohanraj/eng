4e5ccbbc405275a3b4f352410e0212a78588d911
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2024-10-19 16:25:46 -0700

    Implement basic template rendering.

--- eng/generate.py
+++ eng/generate.py
@@ -11,12 +11,27 @@ from eng.config import THE_DAILY_BASE_CONFIG
 from eng.config import THE_DAILY_BASE_DESIGN_CONFIG
 
 PACKAGE_NAME = "eng"
+NEWSLETTER_TEMPLATE_LOCATION = "newsletter.mjml.jinja"
 
 
 def generate_mjml() -> str:
     """
     Generate MJML for a newsletter.
     """
+
+    template_environment = jinja2.Environment(
+        loader=jinja2.PackageLoader(PACKAGE_NAME),
+        autoescape=jinja2.select_autoescape(),
+    )
+
+    template = template_environment.get_template(NEWSLETTER_TEMPLATE_LOCATION)
+
+    return template.render(
+        {
+            "base_config": THE_DAILY_BASE_CONFIG,
+            "base_design_config": THE_DAILY_BASE_DESIGN_CONFIG,
+        }
+    )
 
 
 def generate_html(mjml: str) -> str:
