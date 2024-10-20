f7d5724a7782e074b8810e917e6c0f8e5a5eff3e
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2024-10-19 20:03:02 -0700

    Implement `generate_mjml`.

--- eng/generate.py
+++ eng/generate.py
@@ -5,6 +5,8 @@ and convert that MJML to HTML for an email.
 
 import datetime
 import pathlib
+import subprocess
+import tempfile
 
 import jinja2
 
@@ -13,6 +15,8 @@ from eng.config import THE_DAILY_BASE_DESIGN_CONFIG
 
 PACKAGE_NAME = "eng"
 NEWSLETTER_TEMPLATE_LOCATION = "newsletter.mjml.jinja"
+
+MJML_LOCATION = str(pathlib.Path(".") / "node_modules" / ".bin" / "mjml")
 
 
 def generate_mjml() -> str:
@@ -41,3 +45,21 @@ def generate_html(mjml: str) -> str:
     Generate HTML from MJML source for a newsletter.
     This function expects that MJML has been installed as required.
     """
+
+    # TODO: Consider the free MJML API.
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(mjml.encode())
+        f.flush()
+
+        return subprocess.run(
+            [
+                MJML_LOCATION,
+                f.name,
+                "-s",  # Write output to stdout.
+                "--noStdoutFileComment",
+                "--config.beautify=false",
+                "--config.minify=true",
+            ],
+            capture_output=True,
+            shell=True,
+        ).stdout.decode()
