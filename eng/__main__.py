386abb434e15bdbbbabcbf997dd25a4a79254314
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2024-10-19 20:03:51 -0700

    Implement a basic CLI.

--- eng/__main__.py
+++ eng/__main__.py
@@ -1,3 +1,9 @@
 """
 CLI commands for the package.
 """
+
+from eng.generate import generate_html
+from eng.generate import generate_mjml
+
+with open("index.html", "w") as f:
+    f.write(generate_html(generate_mjml()))
